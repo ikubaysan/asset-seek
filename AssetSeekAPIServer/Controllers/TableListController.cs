6b5eeea5215e4b6d5ccf7ed17776f8ce4a017073
ikubay <ikubaysan@gmail.com>
2025-04-11 19:23:25 -0700

    Add table list endpoint

---
+++ AssetSeekAPIServer/Controllers/TableListController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AssetSeekAPIServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TableListController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TableListController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTables()
+        {
+            var connStr = _configuration.GetConnectionString("AzureSql");
+            var tableNames = new List<string>();
+
+            using var conn = new SqlConnection(connStr);
+            await conn.OpenAsync();
+
+            var cmd = new SqlCommand(
+                @"SELECT TABLE_SCHEMA + '.' + TABLE_NAME 
+                  FROM INFORMATION_SCHEMA.TABLES 
+                  WHERE TABLE_TYPE = 'BASE TABLE'", conn);
+
+            using var reader = await cmd.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                tableNames.Add(reader.GetString(0));
+            }
+
+            return Ok(tableNames);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # asset-seek
 Search engine for Resonite assets
+
+
+
+## Prerequisites
+- Azure CLI installed
+	* `winget install --exact --id Microsoft.AzureCLI`
+	
+
+- Make a copy of appsettings.json.sample and update ConnectionStrings
+	
+	
+
+
+
+Need this so login goes to web browser. Otherwise I get error:
+
+```
+Retrieving tenants and subscriptions for the selection...
+Authentication failed against tenant <guid> 'Default Directory': (pii). Status: Response_Status.Status_InteractionRequired, Error code: 3399614476, Tag: 557973645
+If you need to access subscriptions in the following tenants, please use `az login --tenant TENANT_ID`.
+```
+
+`az config set core.enable_broker_on_windows=false`
+`az login`
